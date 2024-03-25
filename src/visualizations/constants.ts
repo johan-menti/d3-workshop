0fe01f99d5bb5a7c2207ae5648a2286d240999ec
Rafael Belliard <rafael.belliard@mentimeter.com>
2024-03-25 06:58:55 +0100

    feat: push storybook setup

---
+++ src/visualizations/constants.ts
@@ -0,0 +1,63 @@
+import { schemeTableau10 } from "d3-scale-chromatic";
+import { Chart, StoryDatum } from "./types";
+import { colorPalette } from "./utils/color-palettes";
+
+export const INITIAL_DATA = [
+  { label: "ghost", value: 16 },
+  { label: "jalapeño", value: 41 },
+  { label: "habanero", value: 24 },
+  { label: "bell", value: 50 },
+  { label: "scotch", value: 20 },
+];
+
+export const DATA_DEFAULTS = {
+  count: INITIAL_DATA.length,
+  min: 0,
+  max: 50,
+} as const;
+
+export const COMMON_CHART_DEFAULTS = {
+  colorScheme: "tableau 10",
+  colors: schemeTableau10,
+  marginTop: 30,
+  marginRight: 30,
+  marginBottom: 40,
+  marginLeft: 45,
+  transitionDuration: 100,
+  data: INITIAL_DATA,
+  labelAccessor: (d) => d.label,
+  valueAccessor: (d) => d.value,
+} as const satisfies Partial<Chart<StoryDatum>>;
+
+export const COMMON_ARG_TYPES = {
+  colorScheme: {
+    control: "inline-radio",
+    options: Object.keys(colorPalette),
+  },
+  itemCount: {
+    control: {
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+  },
+  maxValue: {
+    control: {
+      type: "range",
+      min: 1,
+      max: 100,
+      step: 1,
+    },
+  },
+  transitionDuration: {
+    control: {
+      min: 1,
+      max: 3000,
+      step: 1,
+    },
+  },
+  colors: { table: { disable: true } },
+  labelAccessor: { table: { disable: true } },
+  valueAccessor: { table: { disable: true } },
+  data: { table: { disable: true } },
+};
